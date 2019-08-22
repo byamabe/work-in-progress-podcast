5e7e63a825ae3ef0670d5dc5589a669598b72ea3
Brian Yamabe <brian@catechize.org>
2019-08-21 19:06:24 -0700

    UPDATE: Make a toolbar to hold the title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
