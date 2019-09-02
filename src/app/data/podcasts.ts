d8170810af1fd8c19e7c0de95bcd42533e433b43
Brian Yamabe <brian@catechize.org>
2019-09-01 22:17:44 -0700

    UPDATE: Fix header margin and episode list layout

--- src/app/data/podcasts.ts
+++ src/app/data/podcasts.ts
@@ -1,7 +1,7 @@
 export const data = {
   episodes: [
-    { id: 1, url: 'http://podcastcom', description: 'This is an episode.', title: 'Episode 1' },
-    { id: 2, url: 'http://podcastcom', description: 'This is an episode again.', title: 'Episode 2' },
-    { id: 3, url: 'http://podcastcom', description: 'This is an episode.', title: 'Episode 3' }
+    { id: 1, url: 'https://www.workinprogresspodcast.com/LGBT.mp3', description: 'We both learn how to better communicate the Christian understanding of LGBT issues. Dad learns how to better communicate without giving offense and Daughter learns how easy misperception is.', title: 'LGBT' },
+    { id: 2, url: 'https://www.workinprogresspodcast.com/Being_Christian.mp3', description: 'We discuss how difficult it is to discuss Christianity outside of Church. Daughter talks about how she would get more support coming out as LGBT than coming out as Chritian at her school. Dad tries to learn how to better communicate with young people.', title: 'Being Christian' },
+    { id: 3, url: 'https://www.workinprogresspodcast.com/NYG.mp3', description: 'We discuss The LCMS National Youth Gathering. Daughter dicsusses what she learned. Dad tries to understand what Daughter learned versus what was trying to be communicated.', title: 'NYG' }
   ]
 };
