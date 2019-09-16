32dda3aa3297d57d25ccaf9e87036563abc2dc2f
Brian Yamabe <brian@catechize.org>
2019-09-15 17:26:36 -0700

    UPDATE: Adjust podcast episode order and upload favicon

--- src/app/data/podcasts.ts
+++ src/app/data/podcasts.ts
@@ -1,8 +1,8 @@
 export const data = {
   episodes: [
     { id: 4, url: 'https://media.workinprogresspodcast.org/Eternal_and_Long-Term.mp3', description: 'We discuss the idea that youth in general don\'t think about the long-term. Daughter discusses the impact of social media on what is perceived to be real and how that affects long-term thinking. Dad realizes how much the background of his childhood affected his long-term thinking.', title: 'Eternal and Long-Term' },
-    { id: 1, url: 'https://media.workinprogresspodcast.org/LGBT.mp3', description: 'We both learn how to better communicate the Christian understanding of LGBT issues. Dad learns how to better communicate without giving offense and Daughter learns how easy misperception is.', title: 'LGBT' },
+    { id: 3, url: 'https://media.workinprogresspodcast.org/NYG.mp3', description: 'We discuss The LCMS National Youth Gathering. Daughter discusses what she learned. Dad tries to understand what Daughter learned versus what was trying to be communicated.', title: 'NYG' },
     { id: 2, url: 'https://media.workinprogresspodcast.org/Being_Christian.mp3', description: 'We discuss how difficult it is to discuss Christianity outside of Church. Daughter talks about how she would get more support coming out as LGBT than coming out as Chritian at her school. Dad tries to learn how to better communicate with young people.', title: 'Being Christian' },
-    { id: 3, url: 'https://media.workinprogresspodcast.org/NYG.mp3', description: 'We discuss The LCMS National Youth Gathering. Daughter discusses what she learned. Dad tries to understand what Daughter learned versus what was trying to be communicated.', title: 'NYG' }
+    { id: 1, url: 'https://media.workinprogresspodcast.org/LGBT.mp3', description: 'We both learn how to better communicate the Christian understanding of LGBT issues. Dad learns how to better communicate without giving offense and Daughter learns how easy misperception is.', title: 'LGBT' }
   ]
 };
